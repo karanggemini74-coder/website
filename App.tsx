7bda95d28b8cd0875d43a6c302619a612bd0c3c4
Karan vijayvargiya <karanggemini74@gmail.com>
2026-04-30 18:10:16 +0530

    feat: Add accessibility widget
    Integrates UserWay accessibility widget for enhanced website accessibility. The widget is dynamically loaded and configured to appear in the bottom-right corner.

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import RegulatoryBanner from './components/RegulatoryBanner';
 import ScrollToTop from './components/ScrollToTop';
 import BlogModal from './components/BlogModal';
+import AccessibilityWidget from './components/AccessibilityWidget';
 
 // Lazy load pages
 const Home = lazy(() => import('./pages/Home'));
@@ -32,6 +33,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <ScrollToTop />
+      <AccessibilityWidget />
       <div className="flex flex-col min-h-screen">
         <RegulatoryBanner />
         <Navbar />
